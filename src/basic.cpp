21c5dd4dbeb4b7c5ba50868338b37e157475ce64
Baileylin <baileylinhfi@gmail.com>
2021-04-08 23:40:28 -0400

    implementing unique images

--- src/basic.cpp
+++ src/basic.cpp
@@ -13,6 +13,32 @@ using namespace glm;
 using namespace agl;
 using namespace std;
 
+//this method is used to implement the original basic.png
+//color ray_color(const ray& r, const hittable_list& world, int depth)
+//{
+//   hit_record rec;
+//   if (depth <= 0)
+//   {
+//      return color(0);
+//   }
+//
+//   if (world.hit(r, 0.001f, infinity, rec))
+//   {
+//      ray scattered;
+//      color attenuation;
+//      if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+//      {
+//         color recurseColor = ray_color(scattered, world, depth - 1);
+//         return attenuation * recurseColor;
+//      }
+//      return attenuation;
+//   }
+//   vec3 unit_direction = normalize(r.direction());
+//   auto t = 0.5f * (unit_direction.y + 1.0f);
+//   return (1.0f - t) * color(1, 1, 1) + t * color(0.5f, 0.7f, 1.0f);
+//}
+
+//this method is used to implement the unique image
 color ray_color(const ray& r, const hittable_list& world, int depth)
 {
    hit_record rec;
@@ -37,6 +63,51 @@ color ray_color(const ray& r, const hittable_list& world, int depth)
    return (1.0f - t) * color(1, 1, 1) + t * color(0.5f, 0.7f, 1.0f);
 }
 
+//this method is used to implement the unique image
+void ray_trace(ppm_image& image)
+{
+    // Image
+    int height = image.height();
+    int width = image.width();
+    float aspect = width / float(height);
+    int samples_per_pixel = 10; // higher => more anti-aliasing
+    int max_depth = 10; // higher => less shadow acne
+
+    // World
+    shared_ptr<material> gray = make_shared<lambertian>(color(0.5f));
+
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5f, gray));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, gray));
+
+    // Camera
+    vec3 camera_pos(0);
+    float viewport_height = 2.0f;
+    float focal_length = 1.0;
+    camera cam(camera_pos, viewport_height, aspect, focal_length);
+
+    // Ray trace
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            color c(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; s++) // antialias
+            {
+                float u = float(i + random_float()) / (width - 1);
+                float v = float(height - j - 1 - random_float()) / (height - 1);
+
+                ray r = cam.get_ray(u, v);
+                c += ray_color(r, world, max_depth);
+            }
+            c = normalize_color(c, samples_per_pixel);
+            image.set_vec3(j, i, c);
+        }
+    }
+
+    image.save("basic.png");
+}
+
 color normalize_color(const color& c, int samples_per_pixel)
 {
    // todo: implement me!
@@ -53,46 +124,47 @@ color normalize_color(const color& c, int samples_per_pixel)
    return color(r, g, b);
 }
 
-void ray_trace(ppm_image& image)
-{
-   // Image
-   int height = image.height();
-   int width = image.width();
-   float aspect = width / float(height);
-   int samples_per_pixel = 10; // higher => more anti-aliasing
-   int max_depth = 10; // higher => less shadow acne
-
-   // World
-   shared_ptr<material> gray = make_shared<lambertian>(color(0.5f));
-
-   hittable_list world;
-   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5f, gray));
-   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, gray));
-
-   // Camera
-   vec3 camera_pos(0);
-   float viewport_height = 2.0f;
-   float focal_length = 1.0; 
-   camera cam(camera_pos, viewport_height, aspect, focal_length);
-
-   // Ray trace
-   for (int j = 0; j < height; j++)
-   {
-      for (int i = 0; i < width; i++)
-      {
-         color c(0, 0, 0);
-         for (int s = 0; s < samples_per_pixel; s++) // antialias
-         {
-            float u = float(i + random_float()) / (width - 1);
-            float v = float(height - j - 1 - random_float()) / (height - 1);
-
-            ray r = cam.get_ray(u, v);
-            c += ray_color(r, world, max_depth);
-         }
-         c = normalize_color(c, samples_per_pixel);
-         image.set_vec3(j, i, c);
-      }
-   }
-
-   image.save("basic.png");
-}
+//this method is used to implement the original basic.png
+//void ray_trace(ppm_image& image)
+//{
+//   // Image
+//   int height = image.height();
+//   int width = image.width();
+//   float aspect = width / float(height);
+//   int samples_per_pixel = 10; // higher => more anti-aliasing
+//   int max_depth = 10; // higher => less shadow acne
+//
+//   // World
+//   shared_ptr<material> gray = make_shared<lambertian>(color(0.5f));
+//
+//   hittable_list world;
+//   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5f, gray));
+//   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, gray));
+//
+//   // Camera
+//   vec3 camera_pos(0);
+//   float viewport_height = 2.0f;
+//   float focal_length = 1.0; 
+//   camera cam(camera_pos, viewport_height, aspect, focal_length);
+//
+//   // Ray trace
+//   for (int j = 0; j < height; j++)
+//   {
+//      for (int i = 0; i < width; i++)
+//      {
+//         color c(0, 0, 0);
+//         for (int s = 0; s < samples_per_pixel; s++) // antialias
+//         {
+//            float u = float(i + random_float()) / (width - 1);
+//            float v = float(height - j - 1 - random_float()) / (height - 1);
+//
+//            ray r = cam.get_ray(u, v);
+//            c += ray_color(r, world, max_depth);
+//         }
+//         c = normalize_color(c, samples_per_pixel);
+//         image.set_vec3(j, i, c);
+//      }
+//   }
+//
+//   image.save("basic.png");
+//}
